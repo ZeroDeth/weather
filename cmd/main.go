38c63a898c5bbe0403374a9133249ecca0383f18
Sherif Abdalla <sherif@abdalla.uk>
2021-04-02 17:00:42 +0100

    Add flag emoji to command line

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -8,18 +9,27 @@ import (
 )
 
 func main() {
+	emoji := flag.Bool("emoji", false, "emoji mod")
+	flag.Parse()
+
 	APIKey := os.Getenv("OPENWEATHERMAP_API_KEY")
+	location := flag.Arg(0)
+	summary, temp, err := weather.Conditions(location, APIKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *emoji {
+		emoji := weather.Emoji(summary)
+		fmt.Printf("%s %.1fC\n", emoji, temp)
+	} else {
+		fmt.Printf("%s %.1fC\n", summary, temp)
+	}
+
 	if APIKey == "" {
 		log.Fatal("OPENWEATHERMAP_API_KEY environment variable must be set")
 	}
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: weather LOCATION (for example, 'weather London')")
 	}
-	location := os.Args[1]
-	summary, temp, err := weather.Conditions(location, APIKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-	emoji := weather.Emoji(summary)
-	fmt.Printf("%s %.1fC\n", emoji, temp)
 }
